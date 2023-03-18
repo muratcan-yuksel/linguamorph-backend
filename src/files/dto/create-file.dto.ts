07badee73dc999d29bd36be08117c59dbe955163
muratcan-yuksel <muratcanyukselpro@gmail.com>
2023-03-18 06:48:31 +0300

    add dtos

--- src/files/dto/create-file.dto.ts
+++ src/files/dto/create-file.dto.ts
@@ -1,1 +1,24 @@
-export class CreateFileDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class CreateFileDto {
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  type: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  url: string;
+
+  @ApiProperty()
+  @IsNumber()
+  @IsNotEmpty()
+  userId: number;
+}
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,26 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsBoolean,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+  IsEmail,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @ApiProperty()
+  @IsEmail()
+  @IsNotEmpty()
+  @MinLength(3)
+  @MaxLength(50)
+  email: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  @MaxLength(50)
+  password: string;
+}
